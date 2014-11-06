3120763d3265ea408c4d2800c07dac0c55e18854
Denis Nurboja <Denis.Nurboja@drtechno.ba>
2014-11-07 00:14:10 +0100

    iteration e3

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -39,8 +39,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart}
         format.json { render action: 'show',
           status: :created, location: @line_item }
       else
@@ -49,6 +48,7 @@ class LineItemsController < ApplicationController
           status: :unprocessable_entity }
       end
     end
+    session[:counter]=nil;
   end
 
   # PATCH/PUT /line_items/1
